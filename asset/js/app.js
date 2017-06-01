b18640c44718df122cb1f5abf2fb8d27e540e7b5
Anna karina Luz <anna.luz.duran@gmail.com>
2017-06-01 15:59:40 -0500

    js funcionalidad

---
+++ asset/js/app.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", function () {
+	// Envío de To - Do
+	var toDoForm = document.getElementById("twitt-text");
+	toDoForm.addEventListener("submit", function (e) {
+		e.preventDefault();
+		// Obtenemos datos
+		var contenedor = document.getElementById("posts");
+		var mensajeContenedor = document.getElementById("text-typo");
+		var mensaje = mensajeContenedor.value;
+
+		// Creamos elementos
+		var postContenedor = document.createElement("article");
+		var post = document.createElement("p");
+
+		// Personalizamos elementos
+		postContenedor.className = "comment";
+		post.textContent = mensaje;
+
+		// Agregarlos al DOM
+		postContenedor.appendChild(post);
+
+		// Agregarlo a un elemento existente para visualizarlo
+		contenedor.appendChild(postContenedor);
+
+		// Borrar contenido de textarea
+		mensajeContenedor.value = "";
+	});
+});
