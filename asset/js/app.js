bb78fcc5524ec74f2a8aed8452ce314cd4f1d45a
Annakluz <anna.luz.duran@gmail.com>
2017-06-04 23:20:30 -0500

    jquery

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,30 +1,56 @@
+(function () { //IIFE
 
-	// Envío de To - Do
-	var toDoForm = document.getElementById("twitt-text");
-	toDoForm.addEventListener("submit", function (e) {
-		e.preventDefault();
-		// Obtenemos datos
-		var contenedor = document.getElementById("posts");
-		var mensajeContenedor = document.getElementById("text-typo");
-		var mensaje = mensajeContenedor.value;
+    var contador = 0;
+    
+    var cargarPagina = function () {
+        $("#twitt-text").submit(agregarTodo);
+        $("#text-typo").keyup(validarContenido);
+    }
+    
+    var agregarTodo = function(e){
+        e.preventDefault();
+        
+         var $contenedor = $("#posts");
+        var $mensajeContenedor = $("#text-typo");
+        var $botonAgregar = $("#send");
+        var mensaje = $mensajeContenedor.val();
 
-		// Creamos elementos
-		var postContenedor = document.createElement("article");
-		var post = document.createElement("p");
+        // Creamos elementos
+        var $postContenedor = $("<article/>", { "class": "comment" });
+        var $post = $("<p/>").append(mensaje);
 
-		// Personalizamos elementos
-		postContenedor.className = "comment";
-		post.textContent = mensaje;
-
-		// Agregarlos al DOM
-		postContenedor.appendChild(post);
-
-		// Agregarlo a un elemento existente para visualizarlo
-		contenedor.appendChild(postContenedor);
-
-		// Borrar contenido de textarea
-		mensajeContenedor.value = "";
-	});
+        // Personalizamos elementos
 
 
-$(document).ready(cargarPagina);
+        // Agregarlos al DOM
+        $postContenedor.append($post);
+
+        // Agregarlo a un elemento existente para visualizarlo
+        $contenedor.prepend($postContenedor);
+
+        // Borrar contenido de textarea
+        mensajeContenedor.value = "";
+        $botonAgregar.attr("disabled" , true);
+        
+        
+        
+    }
+
+
+    // Envío de To - Do
+   /* var toDoForm = document.getElementById("twitt-text");
+    toDoForm.addEventListener("submit", function (e) {
+        e.preventDefault();*/
+        // Obtenemos datos
+       
+
+
+    
+
+
+
+    //FUNCION QUE INICIALIZA 
+
+    $(document).ready(cargarPagina);
+
+})();
